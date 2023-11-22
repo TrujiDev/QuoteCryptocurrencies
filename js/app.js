6f9162d74212c8d03f6933d7d62dc31dc8242aaf
TrujiDev <eetgrisales@gmail.com>
2023-11-22 10:40:35 -0500

    Add form validation and error handling

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
+const currencySelect = document.querySelector('#currency');
 const cryptocurrencySelect = document.querySelector('#cryptocurrency');
+const form = document.querySelector('#form');
+
+const search = {
+	currency: '',
+	cryptocurrency: '',
+};
 
 const getCryptocurrency = cryptocurrencies =>
 	new Promise(resolve => {
@@ -7,6 +14,11 @@ const getCryptocurrency = cryptocurrencies =>
 
 document.addEventListener('DOMContentLoaded', () => {
 	consultCryptocurrency();
+
+	form.addEventListener('submit', submitForm);
+
+	currencySelect.addEventListener('change', readValue);
+	cryptocurrencySelect.addEventListener('change', readValue);
 });
 
 function consultCryptocurrency() {
@@ -27,4 +39,35 @@ function selectCryptocurrency(cryptocurrencies) {
 		option.textContent = FullName;
 		cryptocurrencySelect.appendChild(option);
 	});
+}
+
+function readValue(evt) {
+	search[evt.target.name] = evt.target.value;
+}
+
+function submitForm(evt) {
+	evt.preventDefault();
+
+	const { currency, cryptocurrency } = search;
+
+	if (currency === '' || cryptocurrency === '') {
+		showAlert('Both fields are required');
+		return;
+	}
+}
+
+function showAlert(msg) {
+    const error = document.querySelector('.error');
+
+    if (!error) {
+        const divMsg = document.createElement('div');
+        divMsg.classList.add('error');
+        divMsg.textContent = msg;
+
+        form.appendChild(divMsg);
+
+        setTimeout(() => {
+            divMsg.remove();
+        }, 3000);
+    }
 }
